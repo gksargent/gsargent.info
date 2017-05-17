137f4e28558325b18bca8a0f6b9d062a89eca08a
Greg Sargent <gksargent@gmail.com>
2017-05-17 14:31:55 -0500

    Starting from scratch based on all the cool stuff I've been learning in class.

--- modules/assignments-content.php
+++
@@ -1,5 +0,0 @@
-<h2>Assignment Pages</h2>
-<ul>
-  <li><a href="/assignments/zen-garden-intro.php">Zen Garden Intro</a></li>
-  <li><a href="/assignments/l04-walkthrough.php">Lesson 4 Walkthrough</a></li>
-</ul>
--- modules/header.php
+++
@@ -1,8 +0,0 @@
-<header>
-  <nav>
-    <ul>
-      <li class="brand-mark"><a href="/">Greg Sargent</a></li>
-      <!-- <li><a href="/group-work">Group Work</a></li> -->
-    </ul>
-  </nav>
-</header>
