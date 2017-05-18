5b9f726a0cef3462b400c18f7a314eee1e774d6c
Greg Sargent <gksargent@gmail.com>
2017-05-17 19:41:03 -0500

    V2 Initial commit

---
+++ templates/page-template.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <title>Page Template | Greg Sargent - UX/UI Designer</title>
+    <meta charset="utf-8">
+    <meta name="author" content="Greg Sargent">
+    <meta name="description" content="Greg is a UX/UI Designer in the north Dallas area. Specializing in minimal modern design, Greg is currently working in the FinTech industry designing personal financial management tools at Alkami.">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+  </head>
+
+<body>
+
+  <nav>
+    <ul>
+      <li class="brandMark"><a href="/">Greg Sargent</a></li>
+      <li><a href="/">Link</a></li>
+      <li><a href="/">Link</a></li>
+      <li><a href="/">Link</a></li>
+    </ul>
+  </nav>
+
+  <div id="banner">
+    <h1>Page Title</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
+  </div>
+
+  <main>
+
+    <h1>Lorem Ipsum Dolor Sit Amet Consectetu</h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+
+    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Donec elementum ligula eu sapien consequat eleifend. </p>
+
+    <p>Donec nec dolor erat, condimentum sagittis sem. Praesent porttitor porttitor risus, dapibus rutrum ipsum gravida et. Integer lectus nisi, facilisis sit amet eleifend nec, pharetra ut augue. Integer quam nunc, consequat nec egestas ac, volutpat ac nisi. </p>
+
+  </main>
+
+
+  <footer>
+    <div id="footer-left-column">
+      <p>&copy; Greg Sargent &nbsp; | &nbsp; Proudly coded from scratch.</p>
+    </div>
+    <div id="footer-right-column">
+      <ul>
+        <li><a href="/" target="_blank">icon</a></li>
+      </ul>
+    </div>
+  </footer>
+</body>
+</html>
