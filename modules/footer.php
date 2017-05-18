a7e71c2847d6ed8eba521f8c4456065261224c5f
Greg Sargent <gksargent@gmail.com>
2017-05-17 20:09:20 -0500

    modularized the template page

---
+++ modules/footer.php
@@ -0,0 +1,13 @@
+  <footer>
+
+    <div id="footer-left-column">
+      <p>&copy; Greg Sargent &nbsp; | &nbsp; Proudly coded from scratch.</p>
+    </div>
+
+    <div id="footer-right-column">
+      <ul>
+        <li><a href="/" target="_blank">icon</a></li>
+      </ul>
+    </div>
+
+  </footer>
--- templates/page-template.php
+++ templates/page-template.php
@@ -2,18 +2,9 @@
 <html lang="en-US">
   <head>
     <title>Page Template | Greg Sargent - UX/UI Designer</title>
-    <meta charset="utf-8">
-    <meta name="author" content="Greg Sargent">
     <meta name="description" content="Greg is a UX/UI Designer in the north Dallas area. Specializing in minimal modern design, Greg is currently working in the FinTech industry designing personal financial management tools at Alkami.">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/manifest.json">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="theme-color" content="#ffffff">
-    <link rel="stylesheet" href="/css/main.css">
-    <link rel="stylesheet" href="/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <?php // This php pulls in head.php to create the meta and links
+    include $_SERVER['DOCUMENT_ROOT'].'/modules/head.php'; ?>
   </head>
 
 <body>
@@ -45,15 +36,7 @@
   </main>
 
 
-  <footer>
-    <div id="footer-left-column">
-      <p>&copy; Greg Sargent &nbsp; | &nbsp; Proudly coded from scratch.</p>
-    </div>
-    <div id="footer-right-column">
-      <ul>
-        <li><a href="/" target="_blank">icon</a></li>
-      </ul>
-    </div>
-  </footer>
+  <?php // This php pulls in footer.php to create the footer content
+  include $_SERVER['DOCUMENT_ROOT'].'/modules/footer.php'; ?>
 </body>
 </html>
