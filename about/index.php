8fe56238de8c7cea1ca3cf98771ced6a747c092d
gsargent <gsargent@alkamitech.com>
2017-07-11 19:20:43 -0500

    about page content updated

--- about/index.php
+++ about/index.php
@@ -26,75 +26,124 @@
             <img class="rounded-image centered" src="http://www.gsargent.info/assets/images/_greg-profile.png" width="200" alt="Greg's profile photo">
             <h1 class="centered">Greg Sargent</h1>
             <h3 class="about-section">About Me</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
+            <p>I'm a husband, father of 4 and generally a goofy kinda guy that loves all things digital.</p>
             <h3 class="skills-section">My Skills</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut</p>
+            <p>I'm all in on the Sketch train; fully abandoning the house of Adobe for UI and product design projects.</p>
 
             <div class="skill-container">
-              <p class="skill-title">100% Skill</p>
+              <p class="skill-title">Sketch App</p>
               <div class="skill-outer">
                 <div class="skill-value-10"></div>
               </div><!-- end skill-outer-->
             </div><!-- end skill-container -->
 
             <div class="skill-container">
-              <p class="skill-title">90% Skill</p>
+              <p class="skill-title">Wireframing</p>
               <div class="skill-outer">
                 <div class="skill-value-9"></div>
               </div><!-- end skill-outer-->
             </div><!-- end skill-container -->
 
             <div class="skill-container">
-              <p class="skill-title">80% Skill</p>
+              <p class="skill-title">Mapping User Flows</p>
+              <div class="skill-outer">
+                <div class="skill-value-9"></div>
+              </div><!-- end skill-outer-->
+            </div><!-- end skill-container -->
+
+            <div class="skill-container">
+              <p class="skill-title">Information Architecture</p>
+              <div class="skill-outer">
+                <div class="skill-value-9"></div>
+              </div><!-- end skill-outer-->
+            </div><!-- end skill-container -->
+
+            <div class="skill-container">
+              <p class="skill-title">InVision</p>
+              <div class="skill-outer">
+                <div class="skill-value-9"></div>
+              </div><!-- end skill-outer-->
+            </div><!-- end skill-container -->
+
+            <div class="skill-container">
+              <p class="skill-title">Apple Human Interface Guidelines</p>
               <div class="skill-outer">
                 <div class="skill-value-8"></div>
               </div><!-- end skill-outer-->
             </div><!-- end skill-container -->
 
             <div class="skill-container">
-              <p class="skill-title">70% Skill</p>
+              <p class="skill-title">Material Design Guidelines</p>
               <div class="skill-outer">
                 <div class="skill-value-7"></div>
               </div><!-- end skill-outer-->
             </div><!-- end skill-container -->
 
             <div class="skill-container">
-              <p class="skill-title">60% Skill</p>
+              <p class="skill-title">Screenflow</p>
+              <div class="skill-outer">
+                <div class="skill-value-7"></div>
+              </div><!-- end skill-outer-->
+            </div><!-- end skill-container -->
+
+            <div class="skill-container">
+              <p class="skill-title">HTML</p>
+              <div class="skill-outer">
+                <div class="skill-value-7"></div>
+              </div><!-- end skill-outer-->
+            </div><!-- end skill-container -->
+
+            <div class="skill-container">
+              <p class="skill-title">CSS/SCSS</p>
+              <div class="skill-outer">
+                <div class="skill-value-7"></div>
+              </div><!-- end skill-outer-->
+            </div><!-- end skill-container -->
+
+            <div class="skill-container">
+              <p class="skill-title">InDesign</p>
+              <div class="skill-outer">
+                <div class="skill-value-7"></div>
+              </div><!-- end skill-outer-->
+            </div><!-- end skill-container -->
+
+            <div class="skill-container">
+              <p class="skill-title">Illustrator</p>
               <div class="skill-outer">
                 <div class="skill-value-6"></div>
               </div><!-- end skill-outer-->
             </div><!-- end skill-container -->
 
             <div class="skill-container">
-              <p class="skill-title">50% Skill</p>
+              <p class="skill-title">Jira</p>
               <div class="skill-outer">
                 <div class="skill-value-5"></div>
               </div><!-- end skill-outer-->
             </div><!-- end skill-container -->
 
             <div class="skill-container">
-              <p class="skill-title">40% Skill</p>
+              <p class="skill-title">Photoshop</p>
               <div class="skill-outer">
                 <div class="skill-value-4"></div>
               </div><!-- end skill-outer-->
             </div><!-- end skill-container -->
 
             <div class="skill-container">
-              <p class="skill-title">30% Skill</p>
+              <p class="skill-title">PHP</p>
               <div class="skill-outer">
-                <div class="skill-value-3"></div>
+                <div class="skill-value-4"></div>
               </div><!-- end skill-outer-->
             </div><!-- end skill-container -->
 
             <div class="skill-container">
-              <p class="skill-title">20% Skill</p>
+              <p class="skill-title">Java Script</p>
               <div class="skill-outer">
                 <div class="skill-value-2"></div>
               </div><!-- end skill-outer-->
             </div><!-- end skill-container -->
 
             <div class="skill-container">
-              <p class="skill-title">10% Skill</p>
+              <p class="skill-title">Principle</p>
               <div class="skill-outer">
                 <div class="skill-value-1"></div>
               </div><!-- end skill-outer-->
@@ -107,65 +156,49 @@
                ======================== -->
           <div id="portfolio-right" class="two-thirds column">
             <h3 class="experience-section">My Work</h3>
-            <p class="experience-title">Experience title</p>
-            <p class="experience-meta">Experience meta information</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+            <p class="experience-title">User Interface Designer</p>
+            <p class="experience-meta">Alkami Technology | March 2016 - Present</p>
+            <p>Working on a design scrum team of 4, I participate in two-week sprints to deliver production assets and full high fidelity designs of user flows within Alkami's ORB Online Banking Platform. Efforts are focused on both native iOS/Android apps and a desktop web app solution.</p>
             <ul>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
+              <li>Transitioned UI team from Fireworks to Sketch workflow</li>
+              <li>Established deliverables workflow of Sketch > Zeplin &amp; InVision Inspect</li>
+              <li>Established standardized UI library with 100's of resizable Sketch symbols</li>
+              <li>Established new designer onboarding process</li>
+              <li>Designed exploratory Apple watch concept</li>
+              <li>Designed exploratory concepts for next gen desktop &amp; native mobile apps</li>
+              <li>Introduced incremental updates to the existing legacy design patterns</li>
+              <li>Led the effort to transition away from icon sprites to svg &amp; font icons</li>
+              <li>Introduced the concept of microinteractions for desktop &amp; mobile apps</li>
+              <li>Introduced the concept of the "thumb zone" on native apps</li>
+              <li>100's of user stories completed</li>
             </ul>
             <br>
-            <p class="experience-title">Experience title</p>
-            <p class="experience-meta">Experience meta information</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+            <p class="experience-title">Blogger, Podcaster, Course Designer</p>
+            <p class="experience-meta">Squarespace Guru | December 2013 - March 2016</p>
+            <p>This side project was an effort to generate a recurring passive income stream to compliment my work as a freelancer, and was significant to my development as a designer.</p>
             <ul>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
+              <li>Designed &amp; developed the entire experience</li>
+              <li>10,000 unique monthly visitors</li>
+              <li>5 star-rated weekly audio podcast</li>
+              <li>200+ lesson ecourse with 400+ 'students'</li>
+              <li>$100,000+ revenue generated over 2.5 years</li>
             </ul>
             <br>
-            <p class="experience-title">Experience title</p>
-            <p class="experience-meta">Experience meta information</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+            <p class="experience-title">Web &amp; Print Designer</p>
+            <p class="experience-meta">Freelance | May 2008 - March 2016</p>
+            <p>Starting off coding basic html experiences to offering CMS installs on WordPress and Squarespace, eventually I ran all my projects through Webflow. Projects included work for notable brands like PetSmart, HP Enterprise, Chick-fil-A, Ellie Lines, Rexel, RAFT Architects and many regional &amp; local businesses.</p>
             <ul>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
+              <li>Full stack deisgner on 30+ significant projects</li>
+              <li>100's hours consulting</li>
+              <li>100's collaborative projects</li>
             </ul>
 
             <h3 class="school-section">My Studies</h3>
-            <p class="experience-title">Experience title</p>
-            <p class="experience-meta">Experience meta information</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+            <p class="experience-title">BS in Web Design &amp; Development</p>
+            <p class="experience-meta">Brigham Young University - Idaho | 2015 - 2018</p>
+            <p>Studies focused on both the artistic and scientific aspects of designing for web &amp; mobile devices. Notable courses include human computer interaction, front end programming, advanced CSS, information design, typography, graphic design and communications.</p>
             <ul>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
-            </ul>
-            <br>
-            <p class="experience-title">Experience title</p>
-            <p class="experience-meta">Experience meta information</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
-            <ul>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
-            </ul>
-            </br>
-      <p class="experience-title">Experience title</p>
-            <p class="experience-meta">Experience meta information</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
-            <ul>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
-              <li>Lorem Ipsum Dolor Sit Amet</li>
+              <li>GPA 3.686 - Graduation set for July 2018</li>
             </ul>
           </div>
         </div><!-- end columns -->
